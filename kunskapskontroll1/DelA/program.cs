d654169f769ca5168e48976c50380255031cd32b
Lisette <lisette.ronn@hotmail.com>
2026-09-12 12:30:46 +0200

    strukturerat delarna som ska göras utifrån anvisningarna, satt in listorna och börjat på for-loopen. Även ändrat mappningen för lättare hantering och separering av del A och B.

---
+++ kunskapskontroll1/DelA/program.cs
@@ -0,0 +1,21 @@
+// En lista med namn List<string>
+
+List<string> names = ["Mjölk", "Bröd", "Ost", "Flingor"];
+
+// En lista med priser List<int> - där samma index hör ihop (names[i] kostar prices[i]).
+
+List<int> prices = [15, 32, 89, 25];
+
+
+// Programmet ska snurra i en loop som hela tiden visar listan, totalsumman och ber användaren om input.
+/*Programmet ska hela tiden visa listan som en numrerad lista med totalsumma, t.ex.:
+1. Mjölk - 15 kr
+2. Bröd - 32 kr
+3. Ost - 89 kr
+Totalt: 136 kr
+*/
+
+for(int i = 0; i < names.Count; i++)
+{
+    Console.WriteLine($"{i + 1}. {names[i]}");
+}
